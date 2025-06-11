e51e5a50b0553cfeac54f4bb2d3ecf55cde2f475
Lucjan Gackowski <lucjan.gackowski@gmail.com>
2025-06-11 13:14:12 +0200

    Minor changes to Raport 2.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from model.showcase import clusterize_and_classify, group_and_classify
 def main():
     clusterize_and_classify()
 
-    group_and_classify()
+    #group_and_classify()
 
 
 if __name__ == "__main__":
